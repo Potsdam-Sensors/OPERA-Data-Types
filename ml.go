b7d8aad533ffb887d58fb38e4a32060733ae44a9
Brandon Beattie <brandon@Brandons-MacBook-Pro.local>
2024-10-29 17:37:36 -0400

    more [pm25 ML stuff

--- ml.go
+++ ml.go
@@ -1,5 +1,10 @@
 package operadatatypes
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 /* ~~ Temperature & Humidity ~~ */
 
 // Data going into ML for calculation of flow temp/hum from raw data
@@ -38,6 +43,58 @@ const DATA_TYPE_ML_PRIMARY = "P"
 
 type MlPm25OutputData struct {
 	Pm2p5 float32
+}
+
+type mlPm25InputDataPulses struct {
+	Laser, Pd0, Pd1 uint8
+	Pulses          []Pulse
+}
+
+func (d *mlPm25InputDataPulses) Serialize() []byte {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, d.Laser)
+	binary.Write(buf, binary.LittleEndian, d.Pd0)
+	binary.Write(buf, binary.LittleEndian, d.Pd1)
+	binary.Write(buf, binary.LittleEndian, uint32(len(d.Pulses)))
+	for _, p := range d.Pulses {
+		binary.Write(buf, binary.LittleEndian, p.Height)
+		binary.Write(buf, binary.LittleEndian, p.Width)
+		binary.Write(buf, binary.LittleEndian, p.SidePeak)
+	}
+	return buf.Bytes()
+}
+
+type MlPm25InputData struct {
+	UnixSec   uint32
+	PulseData []mlPm25InputDataPulses
+}
+
+func (d *MlPm25InputData) Serialize() []byte {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, d.UnixSec)
+	binary.Write(buf, binary.LittleEndian, uint32(len(d.PulseData)))
+	for _, p := range d.PulseData {
+		buf.Write(p.Serialize())
+	}
+	return buf.Bytes()
+}
+
+func (d *MlPm25InputData) Populate(t TeensyData) {
+	d.UnixSec = t.UnixSec
+	d.PulseData = make([]mlPm25InputDataPulses, len(t.Counts))
+	for idx, c := range t.Counts {
+		d.PulseData[idx].Laser = c.PinLaser
+		d.PulseData[idx].Pd0 = c.PinPd0
+		d.PulseData[idx].Pd1 = c.PinPd1
+		d.PulseData[idx].Pulses = make([]Pulse, len(c.Pulses))
+		copy(c.Pulses, d.PulseData[idx].Pulses)
+	}
+}
+
+func TeensyDataToMlPm25(t *TeensyData) *MlPm25InputData {
+	ret := &MlPm25InputData{}
+	ret.Populate(*t)
+	return ret
 }
 
 func (d *MlPm25OutputData) DisplayData() *DisplayPrimary {
