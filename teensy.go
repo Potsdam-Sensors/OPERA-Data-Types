dd794ef5529a00be437ecd354f8d6e234ef507c2
Brandon Beattie <brandon@Brandons-MacBook-Pro.local>
2024-12-05 20:45:06 -0500

    new teensy stuff

--- teensy.go
+++ teensy.go
@@ -19,7 +19,7 @@ type TeensyData struct {
 	Counts []*TeensyCounts `json:"counts" binding:"required"`
 }
 
-type Pulse struct { // 12
+type Pulse struct { // 12B
 	Height   float32 `json:"height" binding:"required"`
 	Width    float32 `json:"width" binding:"required"`
 	SidePeak float32 `json:"side_peak" binding:"required"`
@@ -68,4 +68,58 @@ func (d *TeensyData) String() string {
 }
 func (d *TeensyData) SendGob(unixSocketPath string) error {
 	return sendStructGob(d, DATA_TYPE_TEENSY, unixSocketPath)
+}
+
+/* Potentially New Version */
+type NewPulse struct { // 20B
+	Indices  [16]uint16 `json:"indices" binding:"required"`
+	RawPeak  uint16     `json:"raw_peak" binding:"required"`
+	SidePeak uint16     `json:"side_peak"`
+}
+
+type NewTeensyCounts struct { // 55
+	PinPd0   uint8 `json:"pin_pd0" binding:"required"`
+	PinPd1   uint8 `json:"pin_pd1" binding:"required"`
+	PinLaser uint8 `json:"pin_laser" binding:"required"`
+	//3
+
+	RawScalar0    float32 `json:"raw_scalar0" binding:"required"`
+	RawScalar1    float32 `json:"raw_scalar1" binding:"required"`
+	DiffedScalar0 float32 `json:"diffed_scalar0" binding:"required"`
+	DiffedScalar1 float32 `json:"diffed_scalar1" binding:"required"`
+	//16
+
+	Baseline0 float32 `json:"baseline0" binding:"required"`
+	Baseline1 float32 `json:"baseline1" binding:"required"`
+	// 8
+
+	RawUpperTh0    float32 `json:"raw_upper_th0" binding:"required"`
+	RawUpperTh1    float32 `json:"raw_upper_th1" binding:"required"`
+	DiffedUpperTh0 float32 `json:"diffed_upper_th0" binding:"required"`
+	DiffedUpperTh1 float32 `json:"diffed_upper_th1" binding:"required"`
+	//16
+
+	MsRead      uint32 `json:"ms_read" binding:"required"`
+	BuffersRead uint32 `json:"buffers_read" binding:"required"`
+	NumPulses   uint32 `json:"num_pulses" binding:"required"`
+	MaxLaserOn  uint16 `json:"max_laser_on" binding:"required"`
+	// 12
+	PulsesPerSecond float32 `json:"pulses_per_second" binding:"required"`
+	// x
+
+	Pulses []NewPulse `json:"pulses" binding:"required"`
+	// 20 * NumPulses
+}
+
+type NewTeensyData struct {
+	UnixSec   uint32  `json:"unix_sec" binding:"required"`
+	McuTemp   float32 `json:"mcu_temp" binding:"required"`
+	FlowTemp  float32 `json:"flow_temp" binding:"required"`
+	FlowHum   float32 `json:"flow_hum" binding:"required"`
+	FlowRate  float32 `json:"flow_rate" binding:"required"`
+	HvEnabled bool    `json:"hv_enabled" binding:"required"`
+	HvSet     uint8   `json:"hv_set" binding:"required"`
+	HvMonitor uint16  `json:"hv_monitor" binding:"required"`
+
+	Counts []*NewTeensyCounts `json:"counts" binding:"required"`
 }
