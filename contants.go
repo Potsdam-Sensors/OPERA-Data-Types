ae62d206dadae20989f5dbfe520342d12443cc7f
Brandon Beattie <brandon@Brandons-MacBook-Pro.local>
2025-01-22 14:42:24 -0500

    added confidence itnerval to constants

--- contants.go
+++ contants.go
@@ -2,8 +2,9 @@ package operadatatypes
 
 /* Display Settings */
 const (
-	OK_IMX8_TEMP_MAX_C   = 90
-	OK_TEENSY_TEMP_MAX_C = 90
-	OK_LASER_TEMP_MAX_C  = 110
-	OK_FLOW_RATE_MIN     = 0.0
+	OK_IMX8_TEMP_MAX_C      = 90
+	OK_TEENSY_TEMP_MAX_C    = 90
+	OK_LASER_TEMP_MAX_C     = 110
+	OK_FLOW_RATE_MIN        = 0.0
+	CONFIDENCE_INTERVAL_MIN = .8
 )
